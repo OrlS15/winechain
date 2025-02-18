30f599d74e8be4a986302b401caefb9638c9cea3
OrlS15 <orlandosica15@gmail.com>
2025-02-18 13:27:54 +0100

    hardhat config fix

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,14 +7,7 @@ import "@nomicfoundation/hardhat-chai-matchers"
 const config = {
   solidity: "0.8.28",
   networks: {
-    hardhat: {
-      accounts: [
-        {
-          privateKey: process.env.NUXT_BC_PRIVATE_KEY,
-          balance: "100000000000000000000", // 100 ETH in wei
-        },
-      ],
-    },
+    hardhat: {},
   },
 }
 
